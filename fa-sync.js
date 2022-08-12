5cc6838c46dc9e3f2c80deffdf3a16a6e409e5cd
xxxne0xxx <jm82467913@gmail.com>
2022-08-11 17:13:36 -0800

    node commit

--- fa-sync.js
+++
@@ -1,3 +0,0 @@
-const { readFile, writeFile} = require("fs")
-
-readFile("./Node/content/first.txt")
--- file.js
+++
@@ -1,6 +0,0 @@
-const { readFileSync, writeFileSync} = require("fs")
-
-const first = readFileSync("./Node/content/subfolder/second.txt", "utf8")
-console.log(first)
-
-console.log("hello")
